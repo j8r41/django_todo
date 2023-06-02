446e5d8f1e8c307787947d78b78fdc80bf245c6f
j8r41 <kalashnikovroman41@ya.ru>
2023-06-02 19:06:54 +0300

    init commit

---
+++ app/todo/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Task(models.Model):
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ("title",)
+
+    TASK_STATUS = (
+        ("n", "New"),
+        ("ip", "In progress"),
+        ("com", "Completed"),
+        ("can", "Cancelled"),
+    )
+
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(
+        max_length=3,
+        choices=TASK_STATUS,
+        blank=True,
+        default="a",
+    )
+
+    def __str__(self):
+        return self.title
