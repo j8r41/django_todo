dac60d9b78e9272d91413e0190530a24639a0615
j8r41 <kalashnikovroman41@ya.ru>
2023-06-03 18:22:02 +0300

    add popup messages

--- app/todo/views.py
+++ app/todo/views.py
@@ -1,7 +1,9 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib import messages
 
 from .models import Task
 
@@ -17,19 +19,29 @@ class TaskDetailView(DetailView):
     template_name = "todo/task_detail.html"
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(SuccessMessageMixin, CreateView):
     model = Task
     template_name = "todo/task_new.html"
     fields = "__all__"
+    success_url = reverse_lazy("home")
+    success_message = "Task was created successfully."
 
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(SuccessMessageMixin, UpdateView):
     model = Task
     template_name = "todo/task_edit.html"
     fields = "__all__"
+    success_url = reverse_lazy("home")
+    success_message = "Task was updated successfully."
 
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(SuccessMessageMixin, DeleteView):
     model = Task
     template_name = "todo/task_delete.html"
     success_url = reverse_lazy("home")
+    success_message = "Task was deleted successfully."
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        messages.success(self.request, self.success_message)
+        return response
