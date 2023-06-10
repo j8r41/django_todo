686fc32c9bd6f131255a9df370fceacbcd149178
j8r41 <kalashnikovroman41@yandex.ru>
2023-06-10 19:26:41 +0300

    update test

--- app/todo/tests.py
+++ app/todo/tests.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.utils import timezone
 import pytz
 
-from .models import Task
+from .models import Task, Comment
 
 
 class TaskTests(TestCase):
@@ -20,7 +20,7 @@ class TaskTests(TestCase):
             password="secret_password",
         )
 
-        self.task = Task.objects.create(
+        self.task1 = Task.objects.create(
             title="A good title",
             description="A nice description",
             end_date="2023-06-05 11:30:00",
@@ -28,26 +28,35 @@ class TaskTests(TestCase):
             user=self.user1,
             is_deadline_notification_sent=False,
         )
+        self.task2 = Task.objects.create(
+            title="A bad title",
+            description="A bad description",
+            end_date="2023-01-05 23:30:00",
+            status="ip",
+            user=self.user2,
+            is_deadline_notification_sent=False,
+        )
+        self.task1.assigned_users.add(self.user2)
 
     def test_string_representation(self):
-        task = Task(title="A sample title")
-        self.assertEqual(str(task), task.title)
+        task1 = Task(title="A sample title")
+        self.assertEqual(str(task1), task1.title)
 
     def test_get_absolute_url(self):
-        self.assertEqual(self.task.get_absolute_url(), "/task/1/")
+        self.assertEqual(self.task1.get_absolute_url(), "/task/5/")
 
     def test_task_content(self):
-        self.assertEqual(f"{self.task.title}", "A good title")
-        self.assertEqual(f"{self.task.description}", "A nice description")
+        self.assertEqual(f"{self.task1.title}", "A good title")
+        self.assertEqual(f"{self.task1.description}", "A nice description")
         now = timezone.now()
-        self.assertLessEqual(self.task.created_at, now)
+        self.assertLessEqual(self.task1.created_at, now)
         self.assertGreaterEqual(
-            self.task.created_at, now - timedelta(seconds=1)
+            self.task1.created_at, now - timedelta(seconds=1)
         )
-        self.assertEqual(f"{self.task.end_date}", "2023-06-05 11:30:00")
-        self.assertEqual(f"{self.task.status}", "n")
-        self.assertEqual(f"{self.task.user}", "testuser1")
-        self.assertFalse(self.task.is_deadline_notification_sent)
+        self.assertEqual(f"{self.task1.end_date}", "2023-06-05 11:30:00")
+        self.assertEqual(f"{self.task1.status}", "n")
+        self.assertEqual(f"{self.task1.user}", "testuser1")
+        self.assertFalse(self.task1.is_deadline_notification_sent)
 
     def test_login_required_redirect(self):
         response = self.client.get(reverse("home"))
@@ -113,3 +122,45 @@ class TaskTests(TestCase):
         self.client.login(username="testuser1", password="secret_password")
         response = self.client.post(reverse("task_delete", args="1"))
         self.assertEqual(response.status_code, 302)
+
+    def test_assigned_users(self):
+        assigned_users = self.task1.assigned_users.all()
+        self.assertEqual(len(assigned_users), 1)
+        self.assertIn(self.user2, assigned_users)
+
+    def test_assigned_tasks(self):
+        assigned_tasks = self.user2.assigned_tasks.all()
+        self.assertEqual(len(assigned_tasks), 1)
+        self.assertIn(self.task1, assigned_tasks)
+
+
+class CommentTests(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user(
+            username="testuser1",
+            password="secret_password",
+        )
+        self.task1 = Task.objects.create(
+            title="A good title",
+            description="A nice description",
+            end_date="2023-06-05 11:30:00",
+            status="n",
+            user=self.user1,
+            is_deadline_notification_sent=False,
+        )
+        self.comment1 = Comment.objects.create_user(
+            user=self.user1,
+            task=self.task1,
+            text="A good text",
+        )
+
+    def test_string_representation(self):
+        comment1 = Comment(text="A sample text")
+        self.assertEqual(str(comment1), comment1.text)
+
+    def test_task_content(self):
+        self.assertEqual(f"{self.comment1.user}", "testuser1")
+        self.assertEqual(f"{self.comment1.task}", "A good title")
+        self.assertEqual(f"{self.comment1.text}", "A good text")
+        
+    # TODO: Test CRUD CommentModel 
