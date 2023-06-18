59a74ce1837bde4f28798f3888c3579b30c00390
j8r41 <kalashnikovroman41@yandex.ru>
2023-06-18 15:46:59 +0300

    bugfix Changes to be committed: 	modified:   app/context_processors/task_invitations_count.py 	modified:   app/templates/account/logout.html 	modified:   app/templates/includes/header.html

--- app/context_processors/task_invitations_count.py
+++ app/context_processors/task_invitations_count.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import AnonymousUser
+
+
 def task_invitations_count(request):
     return {
-        "task_invitations_count": len(request.user.pending_tasks.all()),
+        "task_invitations_count": len(request.user.pending_tasks.all())
+        if request.user == AnonymousUser
+        else 0,
     }
